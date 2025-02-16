ef7d75cfb18a8c9771e248b4ea4c7f7e5d0255e6
Muhammad Gad <106442148+GADMuhammad@users.noreply.github.com>
2025-02-16 02:14:29 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,15 +18,15 @@ export default function Header() {
 
   return (
     <>
-      <header className="border-b-solid max-five:flex-col flex justify-between gap-4 border-b border-b-veryDarkBlueText px-14 py-8 shadow-one dark:border-b-veryLightGray">
-        <p className="max-five:mx-auto text-2xl font-semibold">
+      <header className="border-b-solid flex justify-between gap-4 border-b border-b-veryDarkBlueText px-14 py-8 shadow-one dark:border-b-veryLightGray max-five:flex-col">
+        <p className="text-2xl font-semibold max-five:mx-auto">
           Where in the world?
         </p>
         <button
           onClick={toggleTheme}
           className="flex items-center justify-center gap-2 border border-solid border-veryDarkBlueText px-2 py-1 tracking-wide dark:border-veryLightGray"
         >
-          <ion-icon name={darkModeCase ? "moon-outline" : "moon"} />
+          <ion-icon name={darkModeCase ? "sunny" : "moon-outline"} />
           Open {darkModeCase ? "Light" : "Dark"} Mood
         </button>
       </header>
